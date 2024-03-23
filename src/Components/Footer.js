19ffd4c59ca1e84f3957696740d579253d660c22
Dinesh <djeeva1515@gmail.com>
2024-03-23 18:49:43 +0530

    changes made

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import {FaGithub, FaLinkedin} from "react-icons/fa";
-import {GrMail} from "react-icons/gr";
-import {SiLeetcode} from "react-icons/si"
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+import { GrMail } from "react-icons/gr";
+import React from "react";
+import { SiLeetcode } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer>
-      <h4>Developed by Devansh Sahni</h4>
-      <h4>Copyright &copy; 2023 DS</h4>
-      <div className='footerLinks'>
-        <a href="https://github.com/DevanshSahni" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/devansh-sahni/" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:devanshsahni@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a>
+      <h4>Copyright &copy; 2023 Dinesh</h4>
+      <div className="footerLinks">
+        <a
+          href="https://github.com/Dineshjeeva"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="www.linkedin.com/in/dineshinlink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="mailTo:djeeva1515@gmail.com" target="_blank" rel="noreferrer">
+          <GrMail />
+        </a>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
