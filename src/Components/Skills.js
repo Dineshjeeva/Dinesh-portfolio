b9158e5d6ea95d359c20c2d90d62a72154c64600
Dinesh <djeeva1515@gmail.com>
2024-03-25 10:26:49 +0530

    added ts logo in about

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,31 +1,38 @@
-import React from 'react'; 
-import { CgCPlusPlus } from "react-icons/cg";
-import {FaReact, FaPython, FaGitAlt, FaGithub, FaNpm, FaFigma, FaBootstrap} from "react-icons/fa";
-import {DiNodejs, DiJavascript1} from "react-icons/di";
-import {SiExpress, SiMongodb, SiPostman, SiVercel} from "react-icons/si";
+import { DiJavascript1, DiNodejs } from "react-icons/di";
+import {
+  FaBootstrap,
+  FaFigma,
+  FaGitAlt,
+  FaGithub,
+  FaNpm,
+  FaReact,
+} from "react-icons/fa";
+import { SiExpress, SiMongodb, SiPostman, SiVercel } from "react-icons/si";
 
-const Skills = ({skill}) => {
-    const icon = {
-        'C++': <CgCPlusPlus/>,
-        Postman: <SiPostman/>,
-        React: <FaReact/>,
-        Javascript: <DiJavascript1/>,
-        Node : <DiNodejs/>,
-        Express : <SiExpress/>,
-        MongoDb : <SiMongodb/>,
-        Git : <FaGitAlt/>,
-        Github : <FaGithub/>,
-        Npm : <FaNpm/>,
-        Figma : <FaFigma/>,
-        Bootstrap: <FaBootstrap/>,
-        Vercel : <SiVercel/>
-    }
-    
+import React from "react";
+
+const Skills = ({ skill }) => {
+  const icon = {
+    Postman: <SiPostman />,
+    React: <FaReact />,
+    Javascript: <DiJavascript1 />,
+    Typescript: "TS",
+    Node: <DiNodejs />,
+    Express: <SiExpress />,
+    MongoDb: <SiMongodb />,
+    Git: <FaGitAlt />,
+    Github: <FaGithub />,
+    Npm: <FaNpm />,
+    Figma: <FaFigma />,
+    Bootstrap: <FaBootstrap />,
+    Vercel: <SiVercel />,
+  };
+
   return (
-    <div title={skill} className='SkillBox'>
+    <div title={skill} className="SkillBox">
       {icon[skill]}
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
