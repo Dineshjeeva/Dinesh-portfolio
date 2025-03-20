3559fa9e32e84cf9152b9e4cfce4a97c4509f6e8
Dinesh <djeeva1515@gmail.com>
2025-03-20 11:42:53 +0530

    projeects ui updated

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -1,52 +1,48 @@
-import React from 'react';
-import {FaGithub} from "react-icons/fa";
-import {CgFileDocument} from "react-icons/cg";
+import { CgFileDocument } from "react-icons/cg";
+import { FaGithub } from "react-icons/fa";
+import React from "react";
 
-
-const  ProjectBox = ({projectPhoto, projectName}) => {
+const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "https://github.com/DevanshSahni/tindog",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
+    Playzo33Github: "",
+    Playzo33Website: "https://playzo33.in",
+  };
 
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
+  const show = desc[projectName + "Github"] ? "inline-block" : "none";
 
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
-    
-    WigglesDesc:"An innovative pet management web app enabling pet parents to create unique pet IDs, securely store and share vaccination records, and generate QR codes for pet profiles, enhancing safety.",
-    WigglesGithub:"https://github.com/DevanshSahni/Wiggles",
-    WigglesWebsite:"https://wiggles.vercel.app/",
-  }
-
-  let show ='';
-  if(desc[projectName + 'Github']===""){
-    show="none";
-  }
-    
   return (
-    <div className='projectBox'> 
-        <img className='projectPhoto' src={projectPhoto} alt="Project display" /> 
-        <div>
-            <br />
-            <h3>{projectName}</h3>
-            <br />
-            {desc[projectName + 'Desc']}
-            <br />
+    <div className="project-box">
+      <div className="relative">
+        <img
+          className="project-image"
+          src={projectPhoto}
+          alt={`${projectName} display`}
+        />
+      </div>
+      <div className="project-content">
+        <h3 className="project-title">{projectName}</h3>
 
-            <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
-              <button className='projectbtn'><FaGithub/> Github</button>
-            </a>
-
-            <a href={desc[projectName + 'Website']} target='_blank'>
-              <button className='projectbtn'><CgFileDocument/> Demo</button>
-            </a>
+        <div className="tech-stack">
+          <span className="tech-chip">React JS</span>
+          <span className="tech-chip">Web Application</span>
+          <span className="tech-chip">TypeScript</span>
         </div>
-    </div>
-  )
-}
 
-export default  ProjectBox
+        <p className="project-desc">{desc[projectName + "Desc"]}</p>
+
+        <div className="action-buttons">
+          {desc[projectName + "Github"] && (
+            <a href={desc[projectName + "Github"]} className="github-button">
+              <FaGithub /> GitHub
+            </a>
+          )}
+          <a href={desc[projectName + "Website"]} className="demo-button">
+            <CgFileDocument /> Demo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectBox;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+import ProjectBox from "./ProjectBox";
+import React from "react";
 
 const Projects = () => {
   return (
     <div>
-      <h1 className='projectHeading'>My <b>Projects</b></h1>
-      <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="Wiggles" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" />
-        <ProjectBox projectPhoto={TindogImage} projectName="Tindog" />
+      <h1 className="projectHeading">
+        Featured <b>Projects</b>
+      </h1>
+      <div className="project">
+        <ProjectBox
+          projectPhoto={
+            "https://atdigitals.com/wp-content/uploads/2024/07/playzoPD2.png"
+          }
+          projectName="Playzo33"
+        />
+        <ProjectBox
+          projectPhoto={
+            "https://upload.wikimedia.org/wikipedia/commons/8/89/Black_Wolf_Supper_Club_in_Eugene%2C_Oregon_%2838111586836%29.jpg"
+          }
+          projectName="blackwolf supper club"
+        />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
