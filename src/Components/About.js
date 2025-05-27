046695270cc319b8f56a0b9ae3c89fc78a0945a9
Dinesh Manikandan TS <104570576+Dineshjeeva@users.noreply.github.com>
2025-05-27 11:20:24 +0530

    home page changes pushed

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,136 +10,108 @@ const About = () => {
       <div className="AboutPage">
         <div className="AboutText">
           <h1>About Meß</h1>
-          <p style={{fontSize:"12px",marginBottom:'2rem'}}>
+          <p style={{ fontSize: "12px", marginBottom: '2rem' }}>
             Get to <b>know</b> me!
           </p>
           <p>
             Front-End Developer | React.js | TypeScript | JavaScript | Redux
-Motivated and detail-oriented Front-End Developer with 3 years of experience building responsive and dynamic web applications. Proficient in modern JavaScript frameworks, primarily React.js, with strong expertise in TypeScript, Redux for state management, and component-driven UI development. Adept at translating UI/UX designs into high-quality code, optimizing performance, and collaborating within Agile teams to deliver scalable, maintainable front-end solutions.
+            Motivated and detail-oriented Front-End Developer with 3 years of experience building responsive and dynamic web applications. Proficient in modern JavaScript frameworks, primarily React.js, with strong expertise in TypeScript, Redux for state management, and component-driven UI development. 
+
           </p>
+          <p style={{margin:'1rem'}}>          Adept at translating UI/UX designs into high-quality code, optimizing performance, and collaborating within Agile teams to deliver scalable, maintainable front-end solutions.
+</p>
         </div>
         <div class='btn-cont'>
-  <a class='contact_me' style={{
-    border:"1px solid"
-  }} href='#'>
- Contact Me    
-  </a>
-</div>
-
-<div class="ag-format-container">
-  <div class="ag-courses_box">
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-          UI/Web&amp;Graph design for teenagers 11-17&#160;years old
+          <a class='contact_me' style={{
+            border: "1px solid"
+          }} href='#'>
+            Contact Me
+          </a>
         </div>
 
-        
-      </a>
-    </div>
+        <div class="ag-format-container">
+          <div class="ag-courses_box">
 
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  React.js
+                </div>
+              </a>
+            </div>
 
-        <div class="ag-courses-item_title">
-          UX/UI Web-Design&#160;+ Mobile Design
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  JavaScript (ES6+)
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  TypeScript
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  HTML5 & CSS3
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  Redux Toolkit / State Management
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  WordPress Development
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  Shopify Customization
+                </div>
+              </a>
+            </div>
+
+            <div class="ag-courses_item">
+              <a href="#" class="ag-courses-item_link">
+                <div class="ag-courses-item_bg"></div>
+                <div class="ag-courses-item_title">
+                  Git & Version Control
+                </div>
+              </a>
+            </div>
+
+          </div>
         </div>
 
-        
-      </a>
-    </div>
 
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
 
-        <div class="ag-courses-item_title">
-          Annual package "Product+UX/UI+Graph designer&#160;2022"
-        </div>
-
-        
-      </a>
-    </div>
-
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-          Graphic Design
-        </div>
-
-       
-      </a>
-    </div>
-
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-          Motion Design
-        </div>
-
-        
-      </a>
-    </div>
-
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-          Front-end development&#160;+ jQuery&#160;+ CMS
-        </div>
-      </a>
-    </div>
-
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg">
-        </div>
-        <div class="ag-courses-item_title">
-          Digital Marketing
-        </div>
-      </a>
-    </div>
-
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-          Interior Design
-        </div>
-
-        
-      </a>
-    </div>
-
-  </div>
-</div>
-
-        
       </div>
 
-      <h1 className="SkillsHeading">Professional Skillset</h1>
-      <div className="skills">
-        <Skills skill="React" />
-        <Skills skill="Node" />
-        <Skills skill="Express" />
-        <Skills skill="MongoDb" />
-        <Skills skill="Git" />
-        <Skills skill="Github" />
-        <Skills skill="Javascript" />
-        <Skills skill="Typescript" />
-        <Skills skill="Postman" />
-        <Skills skill="Npm" />
-        <Skills skill="Bootstrap" />
-      </div>
+
     </>
   );
 };
